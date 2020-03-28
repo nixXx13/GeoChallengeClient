b0af64245dbdf72e862f167990b92e08a55e5612
Nir <nirose@gmail.com>
2020-03-28 17:58:21 +0300

    mixed minor fixes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,27 +9,45 @@ import java.util.Map;
 public class Main {
 
     public static void main(String[] args) throws InterruptedException {
+//        String SERVER_IP = "18.195.215.39";
         String SERVER_IP = "localhost";
         int PORT = 4567;
 
         IGeoChallengeCore gc;
         gc = GeoChallengeCoreFactory.getGeoChallengeCore(SERVER_IP,PORT);
-        gc.registerHandler(mockResponseHandler());
+        gc.registerHandler(mockResponseHandler(gc));
         Thread t = new Thread(gc);
         t.start();
 
+        Thread.sleep(1000);
         gc.send(new GameData(GameData.GameDataType.ACK,"Nir:newRoom:true:1:5:GEO"));
-        Thread.sleep(1000);
-        gc.send(answerGameData("1",1f));
-        Thread.sleep(1000);
-        gc.send(answerGameData("2",1f));
-        Thread.sleep(1000);
-        gc.send(answerGameData("3",1f));
-        Thread.sleep(1000);
-        gc.send(answerGameData("4",1f));
-        Thread.sleep(1000);
-        gc.send(answerGameData("5",1f));
+//        gc.send(new GameData(GameData.GameDataType.ACK,"Nir:TheRoom:true:2:5:GEO"));
+//        gc.send(new GameData(GameData.GameDataType.ACK,"Rose:TheRoom:false:2:5:GEO"));
+        t.join();
+    }
 
+    static IResponseHandler mockResponseHandler(final IGeoChallengeCore gc){
+        return new IResponseHandler() {
+            public void handle(GameData gameData) {
+                 if(gameData.getType().equals(GameData.GameDataType.DATA)){
+                     System.out.println("Received questions! game started");
+                     try {
+                         Thread.sleep(1000);
+                         gc.send(answerGameData("1",1f));
+                         Thread.sleep(1000);
+                         gc.send(answerGameData("2",1f));
+                         Thread.sleep(1000);
+                         gc.send(answerGameData("3",1f));
+                         Thread.sleep(1000);
+                         gc.send(answerGameData("4",1f));
+                         Thread.sleep(1000);
+                         gc.send(answerGameData("5",1f));
+                     } catch (InterruptedException e) {
+                         e.printStackTrace();
+                     }
+                 }
+            }
+        };
     }
 
     public static GameData answerGameData(String answer, Float time){
@@ -37,15 +55,6 @@ public class Main {
         map.put("time",String.valueOf(time));
         map.put("answer",answer);
         return new GameData(GameData.GameDataType.DATA,map);
-    }
-
-    public static IResponseHandler mockResponseHandler(){
-        return new IResponseHandler() {
-            public void handle(GameData gameData) {
-//                System.out.println("recieved " + gameData.toString());
-//                 if data , gc.send
-            }
-        };
     }
 }
 
